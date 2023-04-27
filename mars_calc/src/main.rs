909596ab306381f383c122cb9f903c1aea9fb322
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2023-04-27 10:46:52 +0900

    feat: Weight on Mars with Mecro(println!)

--- mars_calc/src/main.rs
+++ mars_calc/src/main.rs
@@ -1,11 +1,10 @@
 fn main() {
-    println!("Hello, world!");
-
+    println!("Weight on Mars: {}kg", calculate_weight_on_mars(100.0));
     calculate_weight_on_mars(100.0);
 }
 
 
 fn calculate_weight_on_mars(weight: f32) -> f32 {
-    50.0
+    (weight / 9.81) * 3.711
 }
 
