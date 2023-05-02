f87120d0af3445bd23c44f8c762b09e3486dc796
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2023-05-02 23:56:47 +0900

    feat: Enum Case1

--- http_practice/src/main.rs
+++ http_practice/src/main.rs
@@ -1,4 +1,11 @@
 fn main() {
+    let get = Method::GET("abcd".to_string());
+    let delete = Method::DELETE(100);
+    let post = Method::POST;
+    let put = Method::PUT;
+
+
+    
     let server = Server::new("127.0.0.1:8000".to_string());
     server.run();
 }
@@ -17,4 +24,22 @@ impl Server {
     fn run(self) {
         println!("Listening on {}", self.addr);
     }
+}
+
+struct Request {
+    path: String,
+    query_string: String,
+    method: Method,
+}
+
+enum Method {
+    GET(String),
+    DELETE(u64),
+    POST,
+    PUT,
+    HEAD,
+    CONNECT,
+    OPTIONS,
+    TRACE,
+    PATCH,
 }
