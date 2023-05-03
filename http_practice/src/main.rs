a280ef2e184f2030b6a832044d1fffe13b9ceb23
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2023-05-03 22:42:22 +0900

    refactor: 모듈 분리

--- http_practice/src/main.rs
+++ http_practice/src/main.rs
@@ -1,47 +1,11 @@
+mod http;
+mod server;
+
+use server::Server;
+
 fn main() {
-    let server = server::Server::new("127.0.0.1:8000".to_string());
+    let server = Server::new("127.0.0.1:8000".to_string());
     server.run();
 }
 
-mod server {
 
-    pub struct Server {
-        addr: String,
-    }
-
-    impl Server {
-        pub fn new(addr: String) -> Self {
-            Self {
-                addr, // addr: addr 과 같은 의미
-            }
-        }
-
-        pub fn run(self) {
-            println!("Listening on {}", self.addr);
-        }
-    }
-}
-
-mod http {
-    mod request {
-        struct Request {
-            path: String,
-            query_string: Option<String>,
-            method: super::method::Method,
-        }
-    }
-
-    mod method {
-        pub enum Method {
-            GET,
-            DELETE,
-            POST,
-            PUT,
-            HEAD,
-            CONNECT,
-            OPTIONS,
-            TRACE,
-            PATCH,
-        }
-    }
-}
