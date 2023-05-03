2f5fe0df2ce705037f596ec99cadc5029575a0fb
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2023-05-03 21:55:04 +0900

    feat: Module Example

--- http_practice/src/main.rs
+++ http_practice/src/main.rs
@@ -1,42 +1,47 @@
 fn main() {
-    let get = Method::GET;
-    let delete = Method::DELETE;
-    let post = Method::POST;
-    let put = Method::PUT;
-    let server = Server::new("127.0.0.1:8000".to_string());
+    let server = server::Server::new("127.0.0.1:8000".to_string());
     server.run();
 }
 
-struct Server {
-    addr: String,
+mod server {
+
+    pub struct Server {
+        addr: String,
+    }
+
+    impl Server {
+        pub fn new(addr: String) -> Self {
+            Self {
+                addr, // addr: addr 과 같은 의미
+            }
+        }
+
+        pub fn run(self) {
+            println!("Listening on {}", self.addr);
+        }
+    }
 }
 
-impl Server {
-    fn new(addr: String) -> Self {
-        Self {
-            addr, // addr: addr 과 같은 의미
+mod http {
+    mod request {
+        struct Request {
+            path: String,
+            query_string: Option<String>,
+            method: super::method::Method,
         }
     }
 
-    fn run(self) {
-        println!("Listening on {}", self.addr);
+    mod method {
+        pub enum Method {
+            GET,
+            DELETE,
+            POST,
+            PUT,
+            HEAD,
+            CONNECT,
+            OPTIONS,
+            TRACE,
+            PATCH,
+        }
     }
-}
-
-struct Request {
-    path: String,
-    query_string: Option<String>,
-    method: Method,
-}
-
-enum Method {
-    GET,
-    DELETE,
-    POST,
-    PUT,
-    HEAD,
-    CONNECT,
-    OPTIONS,
-    TRACE,
-    PATCH,
 }
