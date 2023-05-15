a806791c4c95c750405cb6be1e4a2e13c941754f
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2023-05-15 23:27:09 +0900

    feat: First Rocket Setting

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+// rocket 크레이트를 사용할 수 있도록 포함하는 것
+#[macro_use] extern crate rocket;
+
+#[get("/")]
+fn hello() -> &'static str {
+    "Hello, world!"
+}
+
+#[rocket::main]
+async fn main() {
+    rocket::build()
+    .mount("/", routes![hello])
+    .launch()
+    .await
+    .unwrap();
 }
