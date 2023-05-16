64949976861f611ec2fca49646395a912dc08eaa
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2023-05-16 23:19:31 +0900

    feat: Base Crud Format

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,48 @@
 #[macro_use] extern crate rocket;
 
 use rocket::serde::json::{Value, json};
+use rocket::response::status;
 
-#[get("/")]
-fn hello() -> Value {
-   json!({
-       "status": "ok",
-       "message": "Hello, world!"
-   })
+#[get("/rustaceans")]
+fn get_rustaceans() -> Value {
+    json!([{ "id": 1, "name": "John Doe" }, { "id": 2, "name": "John Doe again" }])
 }
+
+// 기본적으로 <id>가 매개변수명이다. fn의 매개인자와 일치해야 한다.
+// #은 attribute를 의미한다.
+#[get("/rustaceans/<id>")]
+fn view_rustacean(id: i32) -> Value {
+    json!({"id": id, "name": "John Doe", "email": "john@doe.com"})
+}
+#[post("/rustaceans", format = "json")]
+fn create_rustacean() -> Value {
+    json!({"id": 3, "name": "John Doe", "email": "john@doe.com"})
+}
+#[put("/rustaceans/<id>", format = "json")]
+fn update_rustacean(id: i32) -> Value {
+    json!({"id": id, "name": "John Doe", "email": "john@doe.com"})
+}
+
+// mock 데이터에선 id를 사용하지않아 경고가 뜨므로 _id로 명시한다
+#[delete("/rustaceans/<_id>")]
+fn delete_rustacean(_id: i32) -> status::NoContent {
+    status::NoContent
+}
+
 
 #[rocket::main]
 async fn main() {
     rocket::build()
-    .mount("/", routes![hello])
+    .mount("/", routes![
+        get_rustaceans,
+        view_rustacean,
+        create_rustacean,
+        update_rustacean,
+        delete_rustacean
+    ])
+
     .launch()
     .await
     .unwrap();
 }
+ 
