dc326ee12d7b4ef2be9538eb1df41017fca7f3d4
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2023-05-26 05:02:12 +0900

    feat: Response Base Struct

--- http_practice/src/http/mod.rs
+++ http_practice/src/http/mod.rs
@@ -1,8 +1,10 @@
 pub mod method;
 pub mod query_strings;
 pub mod request;
+pub mod response;
 
 pub use method::Method;
 pub use query_strings::{QueryString, Value as QueryStringValue};
 pub use request::ParseError;
 pub use request::Request;
+pub use response::Response;
