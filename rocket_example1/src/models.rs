49f9f47d8aa293d3c9279eff61aef339fb4a8fcb
youngchan-itall <michael.yc.cho@gmail.com>
2024-08-02 01:00:21 +0900

    feat: 타입 변경

--- rocket_example1/src/models.rs
+++ rocket_example1/src/models.rs
@@ -1,17 +1,19 @@
-use crate::schema::rustaceans;
-use diesel::{Insertable, Queryable};
+use diesel::prelude::*;
+use diesel::{Insertable, Queryable, AsChangeset};
 use serde::{Deserialize, Serialize};
+use chrono::NaiveDateTime;
+use crate::schema::rustaceans;
 
 #[derive(Serialize, Queryable)]
 pub struct Rustacean {
     pub id: i32,
     pub name: String,
     pub email: String,
-    pub created_at: String,
+    pub created_at: NaiveDateTime,  // String -> NaiveDateTime으로 변경
 }
 
-// 삽입 가능한 속성이 필요, 또한 Diesel attr을 통해 어떤 테이블에 삽입할지 명시 해줘야 한다.
-#[derive(Deserialize, Insertable)]
+// 삽입 가능한 속성이 필요하며, Diesel attr을 통해 어떤 테이블에 삽입할지 명시
+#[derive(Deserialize, Insertable, AsChangeset)]
 #[diesel(table_name = rustaceans)]
 pub struct NewRustacean {
     pub name: String,
